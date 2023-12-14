2591c3be37b271e0b9e794bd04c668f6c38258f8
Julio Freitas <julio@draftit.com.br>
2023-12-14 20:17:20 -0300

    publicar

--- src/api/sobre/controllers/sobre.ts
+++
@@ -1,7 +0,0 @@
-/**
- * sobre controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::sobre.sobre');
--- src/api/sobre/services/sobre.ts
+++
@@ -1,7 +0,0 @@
-/**
- * sobre service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::sobre.sobre');
