bd836125464e109e392fe68f4d409dccc687d43d
Julio Freitas <julio@draftit.com.br>
2024-01-03 19:45:17 -0300

    novo

--- script.sh
+++ script.sh
@@ -2,5 +2,6 @@ sudo chmod 777 /var/www/html/run-backend/src/api/avaliacao/documentation/1.0.0/a
 sudo chmod 777 /var/www/html/run-backend/src/api/feedback/documentation/1.0.0/feedback.json
 sudo chmod 777 /var/www/html/run-backend/src/api/categoria/documentation/1.0.0/categoria.json
 sudo chmod 777 /var/www/html/run-backend/src/api/corrida/documentation/1.0.0/corrida.json
+sudo chmod 777 /var/www/html/run-backend/src/api/home/documentation/1.0.0/home.json
 sudo chmod 777 /var/www/html/run-backend/src/api/tipos-de-avaliacao/documentation/1.0.0/tipos-de-avaliacao.json
 sudo chmod 777 /var/www/html/run-backend/src/extensions/documentation/documentation/1.0.0/full_documentation.json
