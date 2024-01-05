c8823ef5517f3d5e80be0aadf945629e12741564
Julio Freitas <julio@draftit.com.br>
2024-01-05 18:10:32 -0300

    again

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  //url: 'https://api.runview.com.br',
+  url: 'https://api.runview.com.br',
   app: {
     keys: env.array('APP_KEYS'),
   },
